39b7c08d37d2dd63ea93bd2598d14761a1f117d7
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-06-14 08:13:03 -0700

    Test pulling in current user on throwaway starting page

--- lib/album_tags_web/controllers/page_controller.ex
+++ lib/album_tags_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule AlbumTagsWeb.PageController do
   use AlbumTagsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", user: conn.assigns.current_user, page: nil)
   end
 end
