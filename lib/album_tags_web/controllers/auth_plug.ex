6d053972797e9598cccc27951747f51b7fb17bfe
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-06-19 10:56:45 -0700

    Simplify formatting and comments

--- lib/album_tags_web/controllers/auth_plug.ex
+++ lib/album_tags_web/controllers/auth_plug.ex
@@ -12,18 +12,15 @@ defmodule AlbumTagsWeb.AuthPlug do
   def call(conn, _params)  do
     user_id = get_session(conn, :user_id)
     cond do
-      # if the user_id exists and the user is in the database
       user = user_id && Accounts.get_user(user_id) ->
         assign(conn, :current_user, user)
-      # if the user doesn't exist on the session OR in the database
       true ->
-        conn
-        |> assign(:current_user, nil)
+        assign(conn, :current_user, nil)
     end
   end
 
   @doc """
-  If no current user is set, redirect the user to the login path. With this module 
+  If no current user is set, redirect the user to the login path. With this module
   in the `album_tags_web.ex` controller pipeline, this method can be used as a plug
   by any controllers that require a user to first be logged in.
   """
