11bfa381f4e6cdba8acee40881b13fe7f0e5f89b
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-06-19 10:56:22 -0700

    URI encode search string before hitting apple music API

--- lib/album_tags_web/controllers/apple_music_controller.ex
+++ lib/album_tags_web/controllers/apple_music_controller.ex
@@ -4,7 +4,8 @@ defmodule AlbumTagsWeb.AppleMusicController do
 
   # /api/apple/search/?search_string=emery&offset=0
   def search(conn, %{"search_string" => search_string, "offset" => offset}) do
-    apple_response = HTTPotion.get("https://api.music.apple.com/v1/catalog/us/search?term=#{search_string}&offset=#{offset || 0}&limit=25&types=artists,albums", headers: [Accept: "application/json", Authorization: "Bearer #{System.get_env("APPLE_MUSIC_TOKEN")}"])
+    clean_search = URI.encode(search_string) # important!
+    apple_response = HTTPotion.get("https://api.music.apple.com/v1/catalog/us/search?term=#{clean_search}&offset=#{offset || 0}&limit=25&types=artists,albums", headers: [Accept: "application/json", Authorization: "Bearer #{System.get_env("APPLE_MUSIC_TOKEN")}"])
 
     render(conn, "index.json", albums: apple_response.body)
   end
