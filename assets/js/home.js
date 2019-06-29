40f9a5649676090366d49cb0931d8a406f83a417
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-06-29 10:12:57 -0700

    Improve home page formatting
    * Add links to user actions

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems, {});
 });
+
+document.getElementsByClassName("open-search-menu")[0].addEventListener("click", function(e) {
+  e.preventDefault();
+  window.sidenav.open();
+});
