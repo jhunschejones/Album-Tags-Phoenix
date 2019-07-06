f4d9099573934c4d9921bc49bf895655d223529d
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-07-05 18:29:07 -0600

    Remove invalid config

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,7 @@ use Mix.Config
 config :album_tags, AlbumTagsWeb.Endpoint,
   url: [scheme: "https", host: "album-tags-phoenix", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json”,
-  https: [compress: true]
+  cache_static_manifest: "priv/static/cache_manifest.json”
 
 # Do not print debug messages in production
 config :logger, level: :info
