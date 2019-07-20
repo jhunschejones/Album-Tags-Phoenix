c2dae731d8711a9fe35a95297e99653be03489de
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-07-20 11:15:09 -0700

    Use stream instead of map for idomatic performance improvement

--- lib/album_tags_web/views/tag_view.ex
+++ lib/album_tags_web/views/tag_view.ex
@@ -3,7 +3,7 @@ defmodule AlbumTagsWeb.TagView do
 
   def tags_for_user(tags, user) do
     tags
-    |> Enum.filter(fn t -> t.user_id == user.id end) # filter to just this user
+    |> Stream.filter(fn t -> t.user_id == user.id end) # filter to just this user
     |> Enum.map(fn tagObject -> %{text: tagObject.text, tag_id: tagObject.id} end) # just return tag text and id
     |> Jason.encode!()
   end
