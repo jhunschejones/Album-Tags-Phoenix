0b8c86c7d7d325048a49bf0adf4945c41b43795e
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-04-03 16:29:31 -0700

    Update elixir and erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AlbumTags.MixProject do
     [
       app: :album_tags,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
